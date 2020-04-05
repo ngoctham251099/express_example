14684c8b56bc9ccf61ac698ccf077af14ffd3745
ngoctham251099 <thamt8974@gmail.com>
2020-04-06 01:49:44 +0700

    Add Side-validation-Unit_12

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 var express = require('express');
-var app = express();
+var pug = require('pug');
+var bodyParser = require('body-parser');
+
+var db = require('./db');
+
+var userRouter = require('./routers/user.router');
 
 var port = 3000;
 
-app.get('/user', function(request, response)
+var app = express();
+app.set('view engine','pug');
+app.set('views','./views');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/', function(req, res)
 {
-	response.send('Hello Coders.Tokyo');
+	res.render('index', {
+		name:'AAA'
+	});
 });
+
+app.use('/users', userRouter);
 
 app.listen(port,function()
 {
-	console.log('Server listening on port' + port);
+	console.log('Server listening on port ' + port);
 });
